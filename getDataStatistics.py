96efee07bbb1405d1ce26e21ae26d54d862187a1
Louis <louis.driffort@gmail.com>
2024-06-13 23:31:32 +0200

    add average in file number stats display

--- getDataStatistics.py
+++ getDataStatistics.py
@@ -1,5 +1,8 @@
-def displayNumberStatistics(data):
-    print(f"max : {max(data)} / min : {min(data)}")
+def displayNumberStatistics(numberList):
+    numberList = [float(num) for num in numberList if num.strip().replace('.', '', 1).isdigit()]
+    average = float(sum(numberList) / len(numberList))
+    print(f"max : {max(numberList)} / min : {min(numberList)} / moyenne : {average:.2f}")
     
-    
+def displayBoolStatistics(boolList):
+        print(boolList)
     
