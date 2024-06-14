9d873fc15531637b6e9b5154eefa18764f1a1d5a
Louis <louis.driffort@gmail.com>
2024-06-14 15:09:54 +0200

    add check if a cell value is a list, add function to transform separated value to list

--- file_helper.py
+++ file_helper.py
@@ -1,17 +1,40 @@
 from error_types import NoExtensionError, UnsupportedFileTypeError
 
   
-def getFileType(fileName):
-    if '.' not in fileName:
+def getFileType(file_name):
+    if '.' not in file_name:
         raise NoExtensionError("Le fichier n'a pas d'extension.")
     
-    fileType = fileName.split('.')[-1]
-    if fileType not in ['csv', 'json']:
-        raise UnsupportedFileTypeError(f"Le type de fichier '{fileType}' n'est pas pris en charge.")
-    return fileType
+    file_type = file_name.split('.')[-1]
+    if file_type not in ['csv', 'json']:
+        raise UnsupportedFileTypeError(f"Le type de fichier '{file_type}' n'est pas pris en charge.")
+    return file_type
+
 
 def getDataType(value):
-    if value.isdigit():
-        return int
+    if is_list(value): 
+            return list 
+    elif value.isdigit():
+        return int 
     elif value.lower() in ['true', 'false']:
         return bool
+
+
+def is_list(value): 
+    if '\n' in value or ',' in value or ';' in value or (value.startswith("[") and value.endswith("]")):
+        return True
+    return False
+
+
+def multiple_string_values_to_list(value):
+    if '\n' in value.lower():
+        value = value.split('\n')
+        
+    if ',' in value.lower():
+        value = value.split(',')
+        
+    if value.startswith("[") and value.endswith("]"):
+        value = value[1:-1]
+        value = multiple_string_values_to_list(value) # rappel avec la valeur sans crochets
+        
+    return value
