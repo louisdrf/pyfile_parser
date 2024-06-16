e6c10a4a5ca2d71e1e684fa1d9701b4fe272222e
goodfoodtruck <mielatomique@protonmail.com>
2024-06-16 15:46:40 +0200

    better list filters

--- data_filtering.py
+++ data_filtering.py
@@ -7,20 +7,30 @@ def filter_on_strings(string_list, value, operation, case_sensitive=False):
     else:
         value_lower = value
     
-    if operation == "contains":
+    if operation == 1:   # contains
         return [string for string in string_list if value_lower in string.lower()]
-    elif operation == "startswith":
+    elif operation == 2: # startswith
         return [string for string in string_list if string.startswith(value_lower)]
-    elif operation == "endswith":
+    elif operation == 3: # endswith
         return [string for string in string_list if string.endswith(value_lower)]
-    elif operation == "size":
+    elif operation == 4: # size
         return [string for string in string_list if len(string) == len(value)]
     else:
         raise ValueError(f"Unknown operation: {operation}")
     
 
-def filter_on_number_of_elements(column, number, file_type):
+def filter_on_list(file_type, column, operation):
     if file_type == 'csv':
-        column = [multiple_string_values_to_list(l) for l in column]
+            column = [multiple_string_values_to_list(l) for l in column]
 
-    return [val for val in column if len(val) == number]
+    if operation == 1: # nombre d'élements
+        number = int(input("Nombre voulu d'éléments dans chaque liste : "))        
+        return [l for l in column if len(l) == number]
+
+    elif operation == 2: # minimum
+        minimum_elem = min(len(liste) for liste in column)
+        return [l for l in column if len(l) == minimum_elem]
+
+    elif operation == 3: # maximum
+        maximum_elem = max(len(liste) for liste in column)
+        return [l for l in column if len(l) == maximum_elem]
